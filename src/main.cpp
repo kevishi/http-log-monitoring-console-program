06a9e0d0f97e21c470a4541c6653dad6c5947a1c
Kevin Shi <tyuiop422@gmail.com>
2018-12-31 01:02:17 -0800

    various fixes in botched copy

--- src/main.cpp
+++ src/main.cpp
@@ -11,14 +11,14 @@ int main(int argc, char *argv[])
   po::options_description desc("Allowed options");
   desc.add_options()
     ("help,h", "display help message")
-    ("file", po::value<string(), "HTTP access log")
+    ("file", po::value<string>(), "HTTP access log")
   ;
   
   po::positional_options_description pDesc;
   pDesc.add("file", 1);
   
   po::variables_map vm;
-  po::store(po::comamnd_line_parser(Argc, argv).options(desc).positional(pDesc).run(), vm);
+  po::store(po::command_line_parser(argc, argv).options(desc).positional(pDesc).run(), vm);
   
   // No required arguments so notify here
   po::notify(vm);
@@ -37,7 +37,7 @@ int main(int argc, char *argv[])
       // character code for celarning screen (same behavior as clear)
       // I would want to extend this using ncurses with mroe time
       cout << "\x1B[2J\x1B[H";
-      cout << vm["file"].as<string() << i << endl;
+      cout << vm["file"].as<string>() << i << endl;
       this_thread::sleep_for(chrono::seconds(1));
     }
   }
